e136ac550dfa93064406042db56591b431a033da
Corbin Ball <c.ball@me.com>
2021-09-24 17:27:06 -0500

    attempt to get authoriztation

--- assets/script.js
+++ assets/script.js
@@ -56,83 +56,40 @@ function resultsOnPage(myArray) {
 
 
 
-var GoogleAuth;
-var SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
-function handleClientLoad() {
-  // Load the API's client and auth2 modules.
-  // Call the initClient function after the modules load.
-  gapi.load('client:auth2', initClient);
-}
-
-function initClient() {
-  // In practice, your app can retrieve one or more discovery documents.
-  var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
-
-  // Initialize the gapi.client object, which app uses to make API requests.
-  // Get API key and client ID from API Console.
-  // 'scope' field specifies space-delimited list of access scopes.
-  gapi.client.init({
-      'apiKey': 'AIzaSyAIk-sQsD_lMEy_rg7tZXsnMV2QP71_ZdsY',
-      'clientId': '10402979075-e52kp9jps4r5gmh9e93sbebvhpj3plu4.apps.googleusercontent.com',
-      'discoveryDocs': [discoveryUrl],
-      'scope': SCOPE
-  }).then(function () {
-    GoogleAuth = gapi.auth2.getAuthInstance();
-
-    // Listen for sign-in state changes.
-    GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-    // Handle initial sign-in state. (Determine if user is already signed in.)
-    var user = GoogleAuth.currentUser.get();
-    setSigninStatus();
-
-    // Call handleAuthClick function when user clicks on
-    //      "Sign In/Authorize" button.
-    $('#sign-in-or-out-button').click(function() {
-      handleAuthClick();
-    });
-    $('#revoke-access-button').click(function() {
-      revokeAccess();
-    });
-  });
-}
-
-function handleAuthClick() {
-  if (GoogleAuth.isSignedIn.get()) {
-    // User is authorized and has clicked "Sign out" button.
-    GoogleAuth.signOut();
-  } else {
-    // User is not signed in. Start Google auth flow.
-    GoogleAuth.signIn();
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
   }
-}
-
-function revokeAccess() {
-  GoogleAuth.disconnect();
-}
-
-function setSigninStatus() {
-  var user = GoogleAuth.currentUser.get();
-  var isAuthorized = user.hasGrantedScopes(SCOPE);
-  if (isAuthorized) {
-    $('#sign-in-or-out-button').html('Sign out');
-    $('#revoke-access-button').css('display', 'inline-block');
-    $('#auth-status').html('You are currently signed in and have granted ' +
-        'access to this app.');
-  } else {
-    $('#sign-in-or-out-button').html('Sign In/Authorize');
-    $('#revoke-access-button').css('display', 'none');
-    $('#auth-status').html('You have not authorized this app or you are ' +
-        'signed out.');
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyAIk-sQsD_lMEy_rg7tZXsnMV2QP71_Zds");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
   }
-}
-
-function updateSigninStatus() {
-  setSigninStatus();
-  console.log(setSigninStatus);
-}
-
-
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "order": "viewCount",
+      "q": "skateboarding dog",
+      "type": [
+        "video"
+      ],
+      "videoDefinition": "high"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+        },
+              function(err) { console.error("Execute error", err); });
+  }
+  //gapi.load("client:auth2", function() {
+    //gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+ // });
 
 
 
