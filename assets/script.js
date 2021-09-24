14f4b84c9334eed62155ce7950d57965f5e9d201
Corbin Ball <c.ball@me.com>
2021-09-24 12:15:44 -0500

    added player video, added youtube api authorization code from google.developers

--- assets/script.js
+++ assets/script.js
@@ -46,17 +46,62 @@ function resultsOnPage(myArray) {
 //document.getElementById('submit').addEventListener('click', youtubeAPI;
 
 function youtubeAPI () {
-    fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&q='+searchInput+'&key=AIzaSyAIk-sQsD_lMEy_rg7tZXsnMV2QP71_Zds')
+    fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=&key=AIzaSyAIk-sQsD_lMEy_rg7tZXsnMV2QP71_Zds')
     .then(function(res){
         console.log(res);
         return res.text();
     })
+    
 }
+
+
+ /**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/guides/code_samples#javascript
+   */
+
+  function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyAIk-sQsD_lMEy_rg7tZXsnMV2QP71_ZdsY");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "maxResults": 25,
+      "q": searchInput
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  //gapi.load("client:auth2", function() {
+    //gapi.auth2.init({client_id: "10402979075-e52kp9jps4r5gmh9e93sbebvhpj3plu4.apps.googleusercontent.com"});
+  //});
+
+  document.getElementById('authorizeLoad').addEventListener('click', authenticate, loadClient);
+  document.getElementById('execute').addEventListener('click', execute);
+
+
+
+
 
 // Search event listener utilizing the 'e' for event
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     displaySearchResults(searchInput.value);
-    youtubeAPI();
 })
