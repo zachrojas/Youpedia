2e8634dc33e7509b9f03f47996b9200cd51b88a5
Corbin Ball <c.ball@me.com>
2021-09-24 23:46:54 -0500

    got youtube api to display

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var resultsList = document.getElementById('resultsList');
 var searchInput = document.getElementById('searchInput');
 var resultsYoutube = document.getElementById('resultsYoutube');
 var searchBtn = document.getElementById('search-btn');
-
+var youtubeFix = [];
 // Function to fetch the search results from Wikipedia
 
 function displaySearchResults(x) {
@@ -60,34 +60,42 @@ function displaySearchResultsYoutube(x) {
     var url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${x}&key=AIzaSyAIk-sQsD_lMEy_rg7tZXsnMV2QP71_Zds`;
     console.log(url);
     fetch(url)
-        .then(function (response) {
-            return (response.json());
-        })
-        .then(function (data) {
-            var resultsArray = data.query.search;
-            resultsOnPagee(resultsArray);
-        })
-        .catch(function () {
-            console.log('There was an error with YouTube API');
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log("this is youtube data")
+                    console.log(data)
+                    console.log(data.items[0].snippet.title)
+                    youtubeFix = data.items
+                    console.log(youtubeFix)
+                    resultsOnPageyoutube(youtubeFix);
+                })
+            } else {
+                console.log('Error: ' + response.statusText);
+            }
+        }).catch(function(error) {
+            console.log('Unable to getForecast: Invalid Connection');
         });
-}
-
+    }
 // Function to display the results
 
-function resultsOnPagee(myArray) {
+function resultsOnPageyoutube(myArray) {
     resultsYoutube.innerHTML = " ";
-    resultsYoutube.insertAdjacentHTML('beforeend', `<h2>Search Results for ${searchInput.value} </h2>`);
+    //resultsYoutube.insertAdjacentHTML('beforeend', `<h2>Search Results for ${searchInput.value} </h2>`);
     myArray.forEach(function (item) {
-        var itemTitle = item.title;
-        var itemSnippet = item.snippet;
-        var itemUrl = encodeURI(`https://www.youtube.com/results?search_query=${item.title}`);
+        console.log(item);
+        var itemTitle = item.snippet.title;
+        console.log(itemTitle);
+        //var itemSnippet = item.snippet;
+        var itemUrl = encodeURI(`https://www.youtube.com/results?search_query=${itemTitle}`);
+        console.log(itemUrl);
         resultsYoutube.insertAdjacentHTML('beforeend',
             `<div class="resultItem">
          <h3 class="resultTitle">
             <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
          </h3>
          <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
-         ${itemSnippet}</a></p>
+         </a></p>
         </div>`
         );
     })
@@ -130,3 +138,19 @@ e.preventDefault();
 displaySearchResults(searchInput.value);
 execute();
 }) */
+
+
+    /* fetch(url)
+        .then(function (response) {
+            console.log("this is the youtube response")
+            return (response.json());
+        })
+        .then(function (data) {
+            console.log("this is data from youtube")
+            var resultsArray = data.query.search;
+            resultsOnPagee(resultsArray);
+        })
+        .catch(function () {
+            console.log('There was an error with YouTube API');
+        }); */
+    //}
